87c9fcdcf82006e59447fd1fbb6282c07a088d04
Yurii Chalov <yuriy.chalov03@gmail.com>
2023-03-09 21:11:22 +0100

    Point: add overloaded operators, make debug defines

---
+++ Point/Point.cpp
@@ -0,0 +1,28 @@
+#ifdef DEBUG
+#include <stdio.h>
+#endif
+
+#include "Point.hpp"
+
+Point& Point::operator=(const Point& a)
+{
+#   ifdef DEBUG
+    printf("ASIIGNMENT\n");
+#   endif
+    if (this == &a)
+        return *this;
+    
+    x = a.x;
+    y = a.y;
+    return *this;
+}
+
+Point operator+(const Point& a, const Point& b)
+{
+    return Point(a.x + b.x, a.y + b.y);
+}
+
+Point operator-(const Point& a, const Point& b)
+{
+    return Point(a.x - b.x, a.y - b.y);
+}
--- Point/Point.hpp
+++ Point/Point.hpp
@@ -1,20 +1,52 @@
 #ifndef POINT_HPP_SENTRY
 #define POINT_HPP_SENTRY
 
+#ifdef DEBUG
+#include <stdio.h>
+#endif
+
 class Point {
     float x, y;
 
 public:
-    Point() : x(0), y(0) {}
-    Point(float _x, float _y) : x(_x), y(_y) {}
-    Point(const Point& a) : x(a.x), y(a.y) {}
-    ~Point() {}
+    Point() : x(0), y(0) 
+    {
+#       ifdef DEBUG
+        printf("DEFAULT CONSTRUCTOR\n");
+#       endif
+    }
+    Point(float _x, float _y) : x(_x), y(_y)
+    {
+#       ifdef DEBUG
+        printf("NON DEFAULT CONSTRUCTOR\n");
+#       endif
+    }
+    Point(const Point& a) : x(a.x), y(a.y)
+    {
+#       ifdef DEBUG
+        printf("COPY CONSTRUCTOR\n");
+#       endif
+    }
+    ~Point()
+    {
+#       ifdef DEBUG
+        printf("DESTRUCTOR\n");
+#       endif
+    }
 
     float GetX() { return x; }
     float GetY() { return y; }
 
     void SetX(float _x) { x = _x; }
     void SetY(float _y) { y = _y; }
+
+    friend Point operator+(const Point& a, const Point& b);
+    friend Point operator-(const Point& a, const Point& b);
+
+    Point& operator=(const Point& a);
 };
+
+Point operator+(const Point& a, const Point& b);
+Point operator-(const Point& a, const Point& b);
 
 #endif
