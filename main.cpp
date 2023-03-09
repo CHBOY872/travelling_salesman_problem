d4c3affa73ea413ac0925f53d7046332049df226
Yurii Chalov <yuriy.chalov03@gmail.com>
2023-03-09 21:11:41 +0100

    update main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+#include "Point/Point.hpp"
+#include "PointHandler/PointHandler.hpp"
+
 int main() 
 {
+    Point a(53,15);
+    Point b(11,53);
+    Point c = a + b;
+    Point d(c + b);
+    Point e = a;
+    Point f(d);
+    c = c + a;
     return 0;
 }
