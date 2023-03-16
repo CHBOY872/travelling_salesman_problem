8a157050af2178ccbc3e5f7acc3a29c63e8c7869
Yurii Chalov <yuriy.chalov03@gmail.com>
2023-03-16 01:07:52 +0100

    PointHandler: update direction

--- PointHandler/PointHandler.cpp
+++
@@ -1,9 +0,0 @@
-#include <math.h>
-
-#include "PointHandler.hpp"
-#include "../Point/Point.hpp"
-
-float PointHandler::Modulo(Point *a, Point *b)
-{
-    return sqrt(pow(b->GetX() - a->GetX(), 2) + pow(b->GetY() - a->GetY(), 2));
-}
