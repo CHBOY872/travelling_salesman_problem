e35c70235f79a67f7d77f6eb1d39ccf3b8a2baa6
Yurii Chalov <yuriy.chalov03@gmail.com>
2023-03-19 01:31:55 +0100

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,22 +6,59 @@
 #include "Point/PointHandler/PointHandler.hpp"
 #include "ProblemSolver/ProblemSolver.hpp"
 
-static NamedPoint points[] = { NamedPoint("A", 1, 1), NamedPoint("B", 5, 8), 
-                               NamedPoint("C", 7, 12), NamedPoint("D", 2, 9),
-                               NamedPoint("E", 7, 2), NamedPoint("F", 1, 12),
-                               NamedPoint("G", 4, 2) };
+static NamedPoint *points = 0;
 
-enum {
-    arr_len = 7
-};
-
-int main() 
+// ./main <point_file> <populations_num> <generations_num> <candidates_num>
+int main(int argc, const char **argv) 
 {
     srand(time(NULL));
+    Genome res;
+    int i, count = 0;
+    char name[10];
+    int x, y;
 
-    Genome res(arr_len);
-    int i;
-    ProblemSolver *pr = ProblemSolver::Init(250, 100, points, arr_len);    
+    if (argc != 5)
+    {
+        fprintf(stderr, "Usage: ./main <point_file> "
+        "<populations_num> <generations_num> <candidates_num>\n");
+        return 1;
+    }
+
+    String point_file = argv[1];
+    int populations_num = atoi(argv[2]);
+    int generations_num = atoi(argv[3]);
+    int candidates_num = atoi(argv[4]);
+
+    FILE *f = fopen(point_file.GetStr(), "r");
+    if (!f)
+    {
+        fprintf(stderr, "Error while opening file: no such file "
+            "or permissions denied\n");
+        return 2;
+    }
+    while (fscanf(f, "%s %d %d", name, &x, &y) != EOF) {
+        count++;
+    }
+    fclose(f);
+
+    points = new NamedPoint[count];
+
+    f = fopen(point_file.GetStr(), "r");
+    if (!f)
+    {
+        fprintf(stderr, "Error while opening file: no such file "
+            "or permissions denied\n");
+        return 2;
+    }
+    i = 0;
+    while (fscanf(f, "%s %d %d", name, &x, &y) != EOF) {
+        points[i] = NamedPoint(name, x, y);
+        i++;
+    }
+    fclose(f);
+
+    ProblemSolver *pr = ProblemSolver::Init(populations_num, generations_num,
+        points, i, candidates_num);    
     pr->Solve(&res);
     for (i = 0; i < res.GetArrayLen(); i++)
         PointHandler::PrintNamedPoint(&res[i]);
@@ -29,5 +66,7 @@ int main()
     printf("%f\n", res.GetLen());
 
     delete pr;
+    delete[] points;
+    points = 0;
     return 0;
 }
